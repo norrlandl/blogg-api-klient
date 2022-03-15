9b68234b85fd97eb5420a29f3d8d074e3cf09d42
norrlandl <lina_holmlund@hotmail.com>
2022-03-15 09:09:18 +0100

    bug fix

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -1,36 +1,30 @@
-window.onload = function(){
-  const form = document.getElementById("main-div")
-  form.addEventListener("submit", postData)
+window.onload = function () {
+  const form = document.getElementById("main-div");
+  form.addEventListener("submit", postData);
 };
 
-
 function postData(e) {
-    e.preventDefault;
+  e.preventDefault();
 
-    const formData = new FormData(e.target);
-    let total = {
-      title: formData.get("title"), 
-      author: formData.get("author"), 
-      content: formData.get("content"), 
-      tags: formData.getAll("tags"),
-    };
-    
-    console.log(total);
-    
+  const formData = new FormData(e.target);
+  let total = {
+    title: formData.get("title"),
+    author: formData.get("author"),
+    content: formData.get("content"),
+    tags: formData.getAll("tags"),
+  };
+
+  console.log(total);
+
   try {
     fetch("http://localhost:5000/posts", {
       method: "POST",
-      headers:{ "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(total),
     });
 
     window.location.replace("../index.html");
-
   } catch (error) {
-  console.log(error);
+    console.log(error);
   }
-
-  
 }
-
